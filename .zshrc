a62d88044a776f7fd15c7ef9e8ddba4b622580ff
Beau Constrictor <dobbob11@duck.com>
2026-05-15 07:51:48 +0100

    intital commit

---
+++ .zshrc
@@ -0,0 +1,52 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="bureau"
+
+CASE_SENSITIVE="true"
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to change how often to auto-update (in days).
+# zstyle ':omz:update' frequency 13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+source $ZSH/oh-my-zsh.sh
+
+plugins=(git fzf-tab)
+
+export PATH="/home/beau/bin:$PATH"
+export PATH=/home/beau/.nimble/bin:$PATH
+export PATH="/home/beau/.local/bin:$PATH"
+
+export LSOPTIONS="-lah"
+export EDTIOR="nvim"
+
+export CEED_INSTALL=~/.local/share/ceed/
+
+alias vi=nvim
+
+unalias ls
+ls() { command ls --color=always -lah "${@:-.}" }
+hex() { xxd -g 1 -Ralways "${@:-.}" }
+
+cpdir() { find "$1" -type f -print0 | while IFS= read -r -d '' f; do; echo "=== $f ==="; cat "$f"; done | wl-copy }
+clear
+sleep 0.5
---
+++ init.lua
@@ -0,0 +1,139 @@
+-- plugins
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "morhetz/gruvbox" },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-telescope/telescope.nvim" },
+  { "wincent/terminus" },
+  { "vim-utils/vim-man" },
+  { "maxbane/vim-asm_ca65" },
+  { "karb94/neoscroll.nvim" },
+  { "zah/nim.vim" },
+})
+
+require("telescope").setup({
+  defaults = {
+    layout_strategy = "horizontal",
+    file_ignore_patterns = { "__pycache__", ".git" },
+  },
+})
+
+require('neoscroll').setup({
+  mappings = {
+    '<C-u>', '<C-d>',
+    '<C-f>', '<C-b>',
+    'zt', 'zz', 'zb',
+  },
+  hide_cursor = true,
+  respect_scrolloff = false,
+  cursor_scrolls_alone = true,
+  duration_multiplier = 0.5,
+  easing = 'cubic',
+  pre_hook = nil,
+  post_hook = nil,
+  performance_mode = false,
+  ignored_events = {
+      'WinScrolled', 'CursorMoved'
+  },
+})
+-- settings
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+
+opt.hidden = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.number = true
+opt.relativenumber = true
+opt.laststatus = 0
+opt.mouse = ""
+
+
+-- keybinds
+
+vim.g.mapleader = " "
+local map = vim.keymap.set
+
+map("n", "<leader>z", "<cmd>bprev<CR>")
+map("n", "<leader>x", "<cmd>bdelete<CR>")
+map("n", "<leader>c", "<cmd>bnext<CR>")
+
+map("n", "<leader>o", "<cmd>Telescope find_files<CR>") 
+
+map("n", "<leader>h", "<cmd>noh<CR>")
+map("n", "<leader>w", "<cmd>w<CR>")
+map("n", "<leader>f", "<cmd>Telescope current_buffer_fuzzy_find<CR>") 
+
+map("n", "<C-m>", ":Vman ")
+
+map("n", "<leader>t", "<cmd>tab terminal<CR>i")
+
+map("n", "<leader>m", "<cmd>tab terminal make -B<CR>")
+map("n", "<leader>r", "<cmd>tab terminal make run<CR>")
+map("n", "<leader>g", "<cmd>tab terminal make dbg<CR>")
+
+map("i", "<S-Tab>", "<C-d>")
+
+local augroup = vim.api.nvim_create_augroup
+
+-- filetypes
+
+-- assembly
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("extension_filetypes", { clear = true }),
+  pattern = { "*.s", "*.inc" },
+  command = "set ft=asm_ca65",
+})
+
+-- indentation
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("language_indentation", { clear = true }),
+  pattern = { "asm_ca65", "c", "nim" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
+-- python
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("py_build", { clear = true }),
+  pattern = "python",
+  callback = function()
+    vim.keymap.set("n", "<leader>m",
+      "<cmd>tab terminal source ./build.sh<CR>",
+      { buffer = true })
+
+    vim.keymap.set("n", "<leader>r",
+      "<cmd>tab terminal source ./run.sh<CR>",
+      { buffer = true })
+  end,
+})
+
+-- aliases
+
+vim.cmd("cabbrev lang Filetypes")
+vim.cmd("cabbrev search Lines")
+
+-- appearance
+
+if vim.g.neovide then
+  vim.opt.guifont = "JetBrainsMono Nerd Font"
+  vim.g.neovide_scale_factor = 0.85
+end
+
+opt.termguicolors = true
+opt.background = "dark"
+vim.cmd("colorscheme gruvbox")
+
+opt.colorcolumn = "70"
+
+opt.shortmess:append("sI")
